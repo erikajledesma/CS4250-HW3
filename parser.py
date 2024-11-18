c3da809ba287b3d7f85682af0d3a2af8cb1581b3
erikajledesma <erika.jasmine06@gmail.com>
2024-11-17 23:45:17 -0800

    can extract professors and their info on one line

--- parser.py
+++ parser.py
@@ -25,5 +25,23 @@ target_content = target['html']
 # use BeautifulSoup to parse through html of target page
 bs = BeautifulSoup(target_content, 'html.parser')
 
+# Use div tag clearfix to find each professor on the page
+
+clearfix = bs.find('div', { 'id': 'main'}).find_all('div', { 'class': 'clearfix'})
+# print(clearfix)
+
+# Regular expression to extract content inside the <p> tags
+# p_tag_pattern = r'<p>(.*?)</p>'
+
+# Iterate through each professor's HTML
+for prof in clearfix:
+    h2_tag = prof.find('h2')
+    prof_name = h2_tag.get_text(strip=True) if h2_tag else "No Name Found"
+
+    p_tag = prof.find('p')
+    p_content = p_tag.get_text(separator=' ', strip=True) if p_tag else "No Details Found"
+
+    print(f"Professor: {prof_name}, P Content: {p_content}")
+
 
 
