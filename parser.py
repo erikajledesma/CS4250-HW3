0bb8cf3ea7b9d1b62b94903ba77e7240d2ec11fc
erikajledesma <erika.jasmine06@gmail.com>
2024-11-17 23:06:21 -0800

    added database connection code

--- parser.py
+++ parser.py
@@ -1,1 +1,19 @@
-# starter code
+import re
+from urllib.request import urlopen
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+from collections import deque
+
+# creating database connection object using pymongo
+
+DB_NAME = "hw3"
+DB_HOST = "localhost"
+DB_PORT = 27017
+
+try:
+    client = MongoClient(host=DB_HOST, port=DB_PORT)
+    db = client[DB_NAME]
+    pages = db['pages']
+    professors = db['professors']
+except:
+    print("Database not connected successfully")
